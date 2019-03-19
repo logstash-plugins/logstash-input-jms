28efef841cbb7ce5c483150d30573bb2ce216f8e
Rob Bavey <rob.bavey@elastic.co>
2019-03-19 10:52:59 -0400

    Rework unit tests

--- spec/inputs/unit/jms_spec.rb
+++ spec/inputs/unit/jms_spec.rb
@@ -1,30 +1,91 @@
-require "logstash/devutils/rspec/spec_helper"
-require "logstash/inputs/jms"
-require "jms"
+require_relative '../spec_helper'
+require 'logstash/inputs/jms'
+require 'jms'
+require 'json'
 
-describe LogStash::Inputs::Jms do
-  let(:queue) { Queue.new }
-  let(:jms_config) {{:require_jars => ["some.jar"],  :jndi_name => "", :jndi_context => {}}}
-  let(:config) do
-    config = { "destination" => "ExampleQueue" }
-    jms_config.each {|k, v| config[k.to_s] = v }
-    config
-  end
-  subject { LogStash::Inputs::Jms.new(config.dup) }
+describe "inputs/jms" do
 
-  context "using default runner (consumer)" do
-    before :each do
-      subject.register
+  describe 'initialization' do
+    let (:yaml_section) { 'activemq' }
+    let (:jms_config) {{'yaml_file' => fixture_path(file), 'yaml_section' => yaml_section, 'destination' => 'ExampleQueue'}}
+
+    context 'via yaml file' do
+      context 'simple yaml configuration' do
+        let (:file) { "jms.yml" }
+
+        it 'should populate jms config from the yaml file' do
+          jms = LogStash::Inputs::Jms.new(jms_config)
+          expect(jms.jms_config).to include({:broker_url => "tcp://localhost:61616",
+                                             :factory=>"org.apache.activemq.ActiveMQConnectionFactory",
+                                             :require_jars=>["activemq-all.jar"]})
+        end
+      end
+
+      context 'jndi yaml configuration' do
+        let (:file) { "jndijms.yml" }
+        let (:yaml_section) { 'solace' }
+        it 'should populate jms config from the yaml file' do
+          jms = LogStash::Inputs::Jms.new(jms_config)
+          expect(jms.jms_config).to include({:jndi_context=>{
+              "java.naming.factory.initial"=>"com.solacesystems.jndi.SolJNDIInitialContextFactory",
+              "java.naming.security.principal"=>"username",
+              "java.naming.provider.url"=>"tcp://localhost:20608",
+              "java.naming.security.credentials"=>"password"},
+                                             :jndi_name => "/jms/cf/default",
+                                             :require_jars => ["commons-lang-2.6.jar",
+                                                               "sol-jms-10.5.0.jar",
+                                                               "geronimo-jms_1.1_spec-1.1.1.jar",
+                                                               "commons-lang-2.6.jar"]})
+        end
+      end
     end
 
-    it "should call the consumer runner" do
-      expect(subject).to receive(:run_consumer).with(queue)
-      subject.run(queue)
+    context 'simple configuration' do
+      let (:jms_config) {{
+          'destination' => 'ExampleQueue',
+          'username' => 'user',
+          'password' => 'the_password',
+          'broker_url' => 'tcp://localhost:61616',
+          'pub_sub' => true,
+          'factory' => 'org.apache.activemq.ActiveMQConnectionFactory',
+          'require_jars' => ['activemq-all-5.15.8.jar']
+      }}
+      it 'should populate jms config from the configuration' do
+        jms = LogStash::Inputs::Jms.new(jms_config)
+        expect(jms.jms_config).to include({:broker_url => "tcp://localhost:61616",
+                                           :factory=>"org.apache.activemq.ActiveMQConnectionFactory",
+                                           :require_jars=>["activemq-all-5.15.8.jar"]})
+      end
     end
+    context 'simple configuration with jndi' do
+      let (:jms_config) {{
+          'destination' => 'ExampleQueue',
+          'jndi_name' => "/jms/cf/default",
+          "jndi_context" => {
+              "java.naming.factory.initial"=>"com.solacesystems.jndi.SolJNDIInitialContextFactory",
+              "java.naming.security.principal"=>"username",
+              "java.naming.provider.url"=>"tcp://localhost:20608",
+              "java.naming.security.credentials"=>"password"},
+          'pub_sub' => true,
+          "require_jars" => ["commons-lang-2.6.jar",
+                             "sol-jms-10.5.0.jar",
+                             "geronimo-jms_1.1_spec-1.1.1.jar",
+                             "commons-lang-2.6.jar"]}}
 
-    it "should create a JMS session based on JMS config" do
-      expect(JMS::Connection).to receive(:session).with(jms_config)
-      subject.run(queue)
+
+      it 'should populate jms config from the configuration' do
+        jms = LogStash::Inputs::Jms.new(jms_config)
+        expect(jms.jms_config).to include({:jndi_context=>{
+            "java.naming.factory.initial"=>"com.solacesystems.jndi.SolJNDIInitialContextFactory",
+            "java.naming.security.principal"=>"username",
+            "java.naming.provider.url"=>"tcp://localhost:20608",
+            "java.naming.security.credentials"=>"password"},
+                                           :jndi_name => "/jms/cf/default",
+                                           :require_jars => ["commons-lang-2.6.jar",
+                                                             "sol-jms-10.5.0.jar",
+                                                             "geronimo-jms_1.1_spec-1.1.1.jar",
+                                                             "commons-lang-2.6.jar"]})
+      end
     end
   end
 end
