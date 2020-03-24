541ee2dd5712d535213af2280a1c9987cf55ebe9
Joao Duarte <jsvduarte@gmail.com>
2020-03-24 14:59:51 +0000

    make tests pass again
    * change use of scp to cp (as docker image doesn't have it)
    * add more debugging to broker bash scripts
    * update activemq download urls as they were broken

--- setup_broker.sh
+++ setup_broker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -xe
 
 if [ -n "${ACTIVEMQ_VERSION+1}" ]; then
   echo "ACTIVEMQ_VERSION is $ACTIVEMQ_VERSION"
@@ -7,8 +7,8 @@ else
    ACTIVEMQ_VERSION=5.15.8
 fi
 
-curl -s -o activemq-all.jar http://central.maven.org/maven2/org/apache/activemq/activemq-all/$ACTIVEMQ_VERSION/activemq-all-$ACTIVEMQ_VERSION.jar
+curl -s -o activemq-all.jar https://repo1.maven.org/maven2/org/apache/activemq/activemq-all/$ACTIVEMQ_VERSION/activemq-all-$ACTIVEMQ_VERSION.jar
 mv activemq-all.jar ./spec/inputs/fixtures/
-curl -s -o activemq-bin.tar.gz  http://apache.mirrors.tds.net/activemq/$ACTIVEMQ_VERSION/apache-activemq-$ACTIVEMQ_VERSION-bin.tar.gz
+curl -s -o activemq-bin.tar.gz http://archive.apache.org/dist/activemq/$ACTIVEMQ_VERSION/apache-activemq-$ACTIVEMQ_VERSION-bin.tar.gz
 tar xvf activemq-bin.tar.gz
 mv apache-activemq-$ACTIVEMQ_VERSION activemq
--- start_broker.sh
+++ start_broker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-scp ./spec/inputs/fixtures/activemq_plaintext.xml activemq/conf/activemq.xml
+cp ./spec/inputs/fixtures/activemq_plaintext.xml activemq/conf/activemq.xml
 activemq/bin/activemq start
