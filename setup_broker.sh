30cea9fe8bcff4d437645472bf1db23cc7386acd
kares <self@kares.org>
2021-11-01 13:03:36 +0100

    CI: use maven central URL for activemq-all.jar

--- setup_broker.sh
+++ setup_broker.sh
@@ -4,11 +4,11 @@ set -e
 if [ -n "${ACTIVEMQ_VERSION+1}" ]; then
   echo "ACTIVEMQ_VERSION is $ACTIVEMQ_VERSION"
 else
-   ACTIVEMQ_VERSION=5.15.8
+   ACTIVEMQ_VERSION=5.15.14
 fi
 
-curl -s -o activemq-all.jar http://central.maven.org/maven2/org/apache/activemq/activemq-all/$ACTIVEMQ_VERSION/activemq-all-$ACTIVEMQ_VERSION.jar
+curl -s -o activemq-all.jar https://repo1.maven.org/maven2/org/apache/activemq/activemq-all/$ACTIVEMQ_VERSION/activemq-all-$ACTIVEMQ_VERSION.jar
 mv activemq-all.jar ./spec/inputs/fixtures/
-curl -s -o activemq-bin.tar.gz  http://apache.mirrors.tds.net/activemq/$ACTIVEMQ_VERSION/apache-activemq-$ACTIVEMQ_VERSION-bin.tar.gz
+curl -s -o activemq-bin.tar.gz http://apache.mirrors.tds.net/activemq/$ACTIVEMQ_VERSION/apache-activemq-$ACTIVEMQ_VERSION-bin.tar.gz
 tar xvf activemq-bin.tar.gz
 mv apache-activemq-$ACTIVEMQ_VERSION activemq
