d1b3dd01840e8b0bf72241defeb6915744254e3e
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2024-05-16 17:04:23 +0000

    tests: fix build
     - use https for fixture resource retrieval
     - use "latest" 5.15.x activemq
     - declare module encapsulation exports

--- setup_broker.sh
+++ setup_broker.sh
@@ -9,6 +9,6 @@ fi
 
 curl -s -o activemq-all.jar https://repo1.maven.org/maven2/org/apache/activemq/activemq-all/$ACTIVEMQ_VERSION/activemq-all-$ACTIVEMQ_VERSION.jar
 mv activemq-all.jar ./spec/inputs/fixtures/
-curl -s -o activemq-bin.tar.gz http://archive.apache.org/dist/activemq/$ACTIVEMQ_VERSION/apache-activemq-$ACTIVEMQ_VERSION-bin.tar.gz
+curl -s -o activemq-bin.tar.gz https://archive.apache.org/dist/activemq/$ACTIVEMQ_VERSION/apache-activemq-$ACTIVEMQ_VERSION-bin.tar.gz
 tar xvf activemq-bin.tar.gz
 mv apache-activemq-$ACTIVEMQ_VERSION activemq
